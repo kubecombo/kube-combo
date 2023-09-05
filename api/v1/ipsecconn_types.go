bf5b695eeb06c5cc0e4fcd29a5bb488ed4e2f23b
bobz965 <zhangbingbing2_yewu@cmss.chinamobile.com>
2023-09-05 10:24:26 +0800

    add vpn gw and ipsec connection api

--- api/v1/ipsecconn_types.go
+++ api/v1/ipsecconn_types.go
@@ -28,26 +28,59 @@ type IpsecConnSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of IpsecConn. Edit ipsecconn_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
 }
 
 // IpsecConnStatus defines the observed state of IpsecConn
 type IpsecConnStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// reference to: https://docs.strongswan.org/docs/5.9/swanctl/swanctlConf.html#_connections
+	VpnGw string `json:"vpnGw"`
+	// Authentication to perform locally.
+	// pubkey uses public key authentication based on a private key associated with a usable certificate. psk uses pre-shared key authentication.
+	// The IKEv1 specific xauth is used for XAuth or Hybrid authentication while the IKEv2 specific eap keyword defines EAP authentication.
+	Auth string `json:"auth"`
+	// 0 accepts both IKEv1 and IKEv2, 1 uses IKEv1 aka ISAKMP, 2 uses IKEv2
+	IkeVersion string `json:"ikeVersion"`
+	// A proposal is a set of algorithms.
+	// For non-AEAD algorithms this includes IKE an encryption algorithm, an integrity algorithm, a pseudo random function (PRF) and a Diffie-Hellman key exchange group.
+	// For AEAD algorithms, instead of encryption and integrity algorithms a combined algorithm is used.
+	// With IKEv2 multiple algorithms of the same kind can be specified in a single proposal, from which one gets selected.
+	// For IKEv1 only one algorithm per kind is allowed per proposal, more algorithms get implicitly stripped. Use multiple proposals to offer different algorithm combinations with IKEv1.
+	//  Algorithm keywords get separated using dashes. Multiple proposals may be separated by commas.
+	// The special value default adds a default proposal of supported algorithms considered safe and is usually a good choice for interoperability. [default]
+	Proposals string `json:"proposals"`
+	// CN is defined in x509 certificate
+	LocalCN string `json:"localCN"`
+	// current public ipsec vpn gw ip
+	LocalPublicIp     string `json:"localPublicIp"`
+	LocalPrivateCidrs string `json:"localPrivateCidrs"`
+
+	RemoteCN string `json:"remoteCN"`
+	// remote public ipsec vpn gw ip
+	RemotePublicIp     string `json:"remotePublicIp"`
+	RemotePrivateCidrs string `json:"remotePrivateCidrs"`
 }
 
 //+kubebuilder:object:root=true
-//+kubebuilder:subresource:status
+// //+kubebuilder:subresource:status
+//+kubebuilder:storageversion
+//+kubebuilder:printcolumn:name="VpnGw",type=string,JSONPath=`.spec.vpnGw`
+//+kubebuilder:printcolumn:name="LocalPublicIp",type=string,JSONPath=`.spec.localPublicIp`
+//+kubebuilder:printcolumn:name="RemotePublicIp",type=string,JSONPath=`.spec.remotePublicIp`
+//+kubebuilder:printcolumn:name="LocalPrivateCidrs",type=string,JSONPath=`.spec.localPrivateCidrs`
+//+kubebuilder:printcolumn:name="RemotePrivateCidrs",type=string,JSONPath=`.spec.remotePrivateCidrs`
+//+kubebuilder:printcolumn:name="LocalCN",type=string,JSONPath=`.spec.localCN`
+//+kubebuilder:printcolumn:name="RemoteCN",type=string,JSONPath=`.spec.remoteCN`
 
 // IpsecConn is the Schema for the ipsecconns API
 type IpsecConn struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   IpsecConnSpec   `json:"spec,omitempty"`
-	Status IpsecConnStatus `json:"status,omitempty"`
+	Spec IpsecConnSpec `json:"spec,omitempty"`
+	// Status IpsecConnStatus `json:"status,omitempty"`  // TODO: add status if needed
 }
 
 //+kubebuilder:object:root=true
--- api/v1/vpngw_types.go
+++ api/v1/vpngw_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -28,18 +29,119 @@ type VpnGwSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of VpnGw. Edit vpngw_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// cpu, memory request
+	// cpu, memory limit
+	// 1C 1G at least
+	Cpu    string `json:"cpu"`
+	Memory string `json:"memory"`
+
+	// 1Mbps bandwidth at least
+	QoSBandwidth string `json:"qosBandwidth"`
+
+	// vpn gw private vpc subnet static ip
+	Ip string `json:"ip"`
+
+	// pod subnet
+	// pod runs the vpn gw server in the specified subnet
+	// user can access all pods in this subnet via vpn gw server
+	Subnet string `json:"subnet"`
+
+	// statefulset replicas
+	Replicas int32 `json:"replicas"`
+
+	// vpn gw pod node selector
+	Selector []string `json:"selector,omitempty"`
+
+	// vpn gw pod tolerations
+	Tolerations []corev1.Toleration `json:"tolerations,omitempty"`
+
+	// vpn gw pod affinity
+	Affinity corev1.Affinity `json:"affinity,omitempty"`
+
+	// vpn gw enable ssl vpn
+	EnableSslVpn bool `json:"enableSslVpn"`
+
+	// ssl vpn secret name, the secret should in the same namespace as the vpn gw
+	SslSecret string `json:"sslSecret,omitempty"`
+
+	// ssl vpn dh secret name, the secret should in the same namespace as the vpn gw
+	DhSecret   string `json:"dhSecret,omitempty"`
+	OvpnCipher string `json:"ovpnCipher"`
+
+	// ssl vpn use openvpn server
+	// all ssl vpn spec start with ovpn
+	// ovpn ssl vpn proto, udp or tcp, udp probably is better
+	OvpnProto string `json:"ovpnProto"`
+	// ovpn ssl vpn port, default 1194 for udp, 443 for tcp
+	OvpnPort int `json:"ovpnPort"`
+
+	// ovpn ssl vpn clinet server subnet cidr 10.240.0.0/16
+	OvpnSubnetCidr string `json:"ovpnSubnetCidr"`
+
+	// ssl vpn server image, use Dockerfile.openvpn
+	SslVpnImage string `json:"sslVpnImage"`
+
+	// pod svc cidr 10.96.0.0/20
+	// OvpnSvcCidr string `json:"ovpnSslVpnSvcCidr"`
+
+	// vpn gw enable ipsec vpn
+	EnableIpsecVpn bool `json:"enableIpsecVpn"`
+
+	// ipsec use strongswan server
+	// all ipsec vpn spec start with ipsec
+	// ipsec vpn secret name, the secret should in the same namespace as the vpn gw
+	IpsecSecret string `json:"ipsecSecret,omitempty"`
+
+	// ipsec vpn local and remote connections, inlude remote ip and subnet
+	IpsecConnections []string `json:"ipsecConnections,omitempty"`
+
+	// ipsec vpn server image, use Dockerfile.strongswan
+	IpsecVpnImage string `json:"ipsecVpnImage"`
 }
 
 // VpnGwStatus defines the observed state of VpnGw
 type VpnGwStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	Cpu              string              `json:"cpu" patchStrategy:"merge"`
+	Memory           string              `json:"memory" patchStrategy:"merge"`
+	QoSBandwidth     string              `json:"qosBandwidth" patchStrategy:"merge"`
+	Ip               string              `json:"ip" patchStrategy:"merge"`
+	Subnet           string              `json:"subnet" patchStrategy:"merge"`
+	Replicas         int32               `json:"replicas" patchStrategy:"merge"`
+	Selector         []string            `json:"selector,omitempty" patchStrategy:"merge"`
+	Tolerations      []corev1.Toleration `json:"tolerations,omitempty" patchStrategy:"merge"`
+	Affinity         corev1.Affinity     `json:"affinity,omitempty" patchStrategy:"merge"`
+	EnableSslVpn     bool                `json:"enableSslVpn" patchStrategy:"merge"`
+	SslSecret        string              `json:"sslSecret"  patchStrategy:"merge"`
+	DhSecret         string              `json:"dhSecret"  patchStrategy:"merge"`
+	SslVpnImage      string              `json:"sslVpnImage" patchStrategy:"merge"`
+	OvpnCipher       string              `json:"ovpnCipher" patchStrategy:"merge"`
+	OvpnProto        string              `json:"ovpnProto" patchStrategy:"merge"`
+	OvpnPort         int                 `json:"ovpnPort" patchStrategy:"merge"`
+	OvpnSubnetCidr   string              `json:"ovpnSubnetCidr" patchStrategy:"merge"`
+	EnableIpsecVpn   bool                `json:"enableIpsecVpn" patchStrategy:"merge"`
+	IpsecSecret      string              `json:"ipsecSecret"  patchStrategy:"merge"`
+	IpsecVpnImage    string              `json:"ipsecVpnImage" patchStrategy:"merge"`
+	IpsecConnections []string            `json:"ipsecConnections,omitempty" patchStrategy:"merge"`
+
+	// Conditions store the status conditions of the vpn gw instances
+	// +operator-sdk:csv:customresourcedefinitions:type=status
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,1,rep,name=conditions"`
 }
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
+//+kubebuilder:storageversion
+//+kubebuilder:printcolumn:name="IP",type=string,JSONPath=`.spec.ip`
+//+kubebuilder:printcolumn:name="PublicIP",type=string,JSONPath=`.spec.publicIp`
+//+kubebuilder:printcolumn:name="Subnet",type=string,JSONPath=`.spec.subnet`
+//+kubebuilder:printcolumn:name="Cpu",type=string,JSONPath=`.spec.cpu`
+//+kubebuilder:printcolumn:name="Mem",type=string,JSONPath=`.spec.memory`
+//+kubebuilder:printcolumn:name="QoS",type=string,JSONPath=`.spec.qoSBandwidth`
+//+kubebuilder:printcolumn:name="EnableSsl",type=string,JSONPath=`.spec.enableSslVpn`
+//+kubebuilder:printcolumn:name="EnableIpsec",type=string,JSONPath=`.spec.enableIpsecVpn`
 
 // VpnGw is the Schema for the vpngws API
 type VpnGw struct {
