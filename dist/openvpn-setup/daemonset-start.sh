c400213d39f966f2bcf7e16b7f2c99aed0d50c5b
zbb88888 <zhangbingbing2_yewu@cmss.chinamobile.com>
2025-02-23 13:27:58 +0000

    fix: static pod start
    Signed-off-by: GitHub <noreply@github.com>

--- dist/openvpn-setup/daemonset-start.sh
+++ dist/openvpn-setup/daemonset-start.sh
@@ -12,6 +12,7 @@ bash -x "${SETUP_HOME}/configure.sh"
 rm -fr "$CONF_HOME/host-init/*"
 
 # copy all openvpn server need file to host-init
+\cp "$SETUP_HOME/static-pod-start.sh" "$CONF_HOME/host-init/"
 \cp "$CONF_HOME/openvpn.conf" "$CONF_HOME/host-init/"
 \cp -r "$CONF_HOME/certs" "$CONF_HOME/host-init/"
 \cp -L "$CONF_HOME/dh/dh.pem" "$CONF_HOME/host-init/"
@@ -19,9 +20,12 @@ rm -fr "$CONF_HOME/host-init/*"
 echo "show $CONF_HOME/host-init/ files .............."
 ls -lR "$CONF_HOME/host-init/"
 
+echo "k8s static pod should run $CONF_HOME/host-init/static-pod-start.sh .............."
+echo "k8s static pod will copy certs and config file from host-init to pod /etc/openvpn .............."
+echo "k8s static pod will run openvpn --config /etc/openvpn/openvpn.conf .............."
 echo "alreay setup ssl vpn certs and config, sleep, you can use this pod to debug .............."
-echo "you can use this pod to debug later .............."
+echo "this is also a debug pod .............."
+echo "sleep .............."
 sleep infinity
 
 # k8s static pod will copy file from this host-init to pod /etc/openvpn
-# and start openvpn server
