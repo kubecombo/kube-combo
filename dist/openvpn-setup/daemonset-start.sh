249c23657180fec22801a53eb13127d89b069d00
zbb88888 <zhangbingbing2_yewu@cmss.chinamobile.com>
2025-02-25 02:30:14 +0000

    ds auto deploy static pod
    Signed-off-by: GitHub <noreply@github.com>

--- dist/openvpn-setup/daemonset-start.sh
+++ dist/openvpn-setup/daemonset-start.sh
@@ -11,14 +11,37 @@ bash -x "${SETUP_HOME}/configure.sh"
 # clean up openvpn certs and conf cache dir /etc/host-init-openvpn
 rm -fr "/etc/host-init-openvpn/*"
 
+while [ ! -f "${SETUP_HOME}/static-pod-start.sh" ]; do
+	sleep 1
+	echo "waiting for ${SETUP_HOME}/static-pod-start.sh ............"
+done
+
+while [ ! -f "${SETUP_HOME}/static-openvpn.yaml" ]; do
+	sleep 1
+	echo "waiting for ${SETUP_HOME}/static-openvpn.yaml ............"
+done
+
+while [ ! -d "${CONF_HOME}/openvpn.conf" ]; do
+	sleep 1
+	echo "waiting for ${CONF_HOME}/openvpn.conf ............"
+done
+
+while [ ! -f "${CONF_HOME}/dh/dh.pem" ]; do
+	sleep 1
+	echo "waiting for ${CONF_HOME}/dh/dh.pem ............"
+done
+
 # copy all openvpn server need file from /etc/openvpn to /etc/host-init-openvpn
-\cp "$SETUP_HOME/static-pod-start.sh" "/etc/host-init-openvpn"
-\cp "$CONF_HOME/openvpn.conf" "/etc/host-init-openvpn"
-\cp -r "$CONF_HOME/certs" "/etc/host-init-openvpn"
-\cp -L "$CONF_HOME/dh/dh.pem" "/etc/host-init-openvpn"
+\cp "${SETUP_HOME}/static-pod-start.sh" "/etc/host-init-openvpn"
+\cp "${CONF_HOME}/openvpn.conf" "/etc/host-init-openvpn"
+\cp -r "${CONF_HOME}/certs" "/etc/host-init-openvpn"
+\cp -L "${CONF_HOME}/dh/dh.pem" "/etc/host-init-openvpn"
 
 echo "show /etc/host-init-openvpn files .............."
 ls -lR "/etc/host-init-openvpn"
+
+echo "deploy static pod /etc/kubernetes/manifests .............."
+\cp "${SETUP_HOME}/static-openvpn.yaml" "/etc/kubernetes/manifests"
 
 echo "k8s static pod should run /etc/host-init-openvpn/static-pod-start.sh .............."
 echo "k8s static pod will copy certs and config file from /etc/host-init-openvpn to pod /etc/openvpn .............."
