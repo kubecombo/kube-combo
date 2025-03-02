8c6a9a056adb77c6041c565358e0bd5d50b66ab8
zbb88888 <jmdxjsjgcxy@gmail.com>
2025-03-02 10:11:50 +0000

    keepalived: show conf
    Signed-off-by: GitHub <noreply@github.com>

--- dist/keepalived-setup/configure.sh
+++ dist/keepalived-setup/configure.sh
@@ -44,6 +44,9 @@ cp "${TEMPLATE_CONF}" keepalived.conf.j2
 # use j2 to generate keepalived.conf
 j2 keepalived.conf.j2 "${VALUES_YAML}" -o "${CONF}"
 
+echo "keepalived.conf:"
+cat "${CONF}"
+
 echo "start keepalived..."
 host=$(hostname)
 /usr/sbin/keepalived --log-console --log-detail --dont-fork --config-id="${host}" --use-file="${CONF}"
