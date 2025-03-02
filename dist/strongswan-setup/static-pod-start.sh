b217f203684f806849b61b9c866c60b33af6be56
zbb88888 <jmdxjsjgcxy@gmail.com>
2025-03-02 15:11:26 +0000

    fix: sync ipsec static yaml
    Signed-off-by: GitHub <noreply@github.com>

--- dist/strongswan-setup/static-pod-start.sh
+++ dist/strongswan-setup/static-pod-start.sh
@@ -13,28 +13,35 @@ READY_FLAG="STRONGSWAN_CONTENT_END"
 
 # wait connection is ready and then copy it
 # loop to check if hosts has connection
+echo "check if has ${CACHE_HOME}/swanctl.conf ............"
+while [ ! -f "${CACHE_HOME}/swanctl.conf" ]; do
+	echo "waiting for ${CACHE_HOME}/swanctl.conf ............"
+	sleep 5
+done
+echo "found ${CACHE_HOME}/swanctl.conf ............"
+cat "${CACHE_HOME}/swanctl.conf"
+
+echo "check if ${CACHE_HOME}/hosts ready ............"
 while true; do
-    if grep -q "$READY_FLAG" /etc/hosts; then  
-        echo "found connections: $READY_FLAG"
+    if grep -q "${READY_FLAG}" "${CACHE_HOME}/hosts"; then
+        echo "found connections: ${READY_FLAG}"
         break
     else
-        echo "not found connections: $READY_FLAG, waiting"
+        echo "not found connections: ${READY_FLAG}, waiting"
         sleep 5
-    fi  
-done  
-while [ ! -f "${CONF_HOME}/swanctl.conf" ]; do
-	echo "waiting for ${CONF_HOME}/swanctl.conf ............"
-	sleep 5
+    fi
 done
 
 # clean up old ipsecvpn certs and conf cache to use new in /etc/host-init-ipsecvpn
 rm -fr "/etc/swanctl/*"
 
-\cp -r "${CACHE_HOME}/*" "${CONF_HOME}/"
-\cp "${CACHE_HOME}/hosts" "${HOSTS_HOME}" 
+\cp -r "${CACHE_HOME}/private" "${CONF_HOME}/"
+\cp -r "${CACHE_HOME}/x509" "${CONF_HOME}/"
+\cp -r "${CACHE_HOME}/x509ca" "${CONF_HOME}/"
 
-echo "show ${CONF_HOME} files .............."
-ls -lR "${CACHE_HOME}/"
+\cp "${CACHE_HOME}/swanctl.conf" "${CONF_HOME}/"
+
+\cp "${CACHE_HOME}/hosts" "${CONF_HOME}/"
 
 # start ipsecvpn server
 echo "Running ipsecvpn .............."
