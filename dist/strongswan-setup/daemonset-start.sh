49056957d5de970e0fcb721483aa13515ecd2c54
zbb88888 <jmdxjsjgcxy@gmail.com>
2025-03-03 01:37:06 +0000

    simple ipsec ds start sh
    Signed-off-by: GitHub <noreply@github.com>

--- dist/strongswan-setup/daemonset-start.sh
+++ dist/strongswan-setup/daemonset-start.sh
@@ -2,7 +2,5 @@
 set -eux
 
 echo "k8s static pod should run /etc/host-init-ipsecvpn/static-pod-start.sh .............."
-echo "k8s static pod will copy host, certs, config file from /etc/host-init-ipsecvpn to pod /etc/swanctl .............."
-echo "this is also a debug pod .............."
 echo "sleep .............."
 sleep infinity
