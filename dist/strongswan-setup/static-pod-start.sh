1ff07eaceb1031a94333390ca5fd05119f8b3fff
zbb88888 <jmdxjsjgcxy@gmail.com>
2025-03-14 12:57:28 +0800

    Ipsec test (#64)
    * ipsec static not need hosts
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * debug
    
    Signed-off-by: GitHub <noreply@github.com>
    
    ---------
    
    Signed-off-by: GitHub <noreply@github.com>

--- dist/strongswan-setup/static-pod-start.sh
+++ dist/strongswan-setup/static-pod-start.sh
@@ -9,25 +9,12 @@ set -eux
 # make it runable in any directory
 CACHE_HOME=${CACHE_HOME:-/etc/host-init-strongswan}
 CONF_HOME=${CONF_HOME:-/etc/swanctl}
-HOSTS_HOME=${HOSTS_HOME:-/etc/hosts}
-# use READY_FLAG to check /etc/hosts is aleady has connections
-READY_FLAG="STRONGSWAN_CONTENT_END"
 
 # wait connection is ready and then copy it
 # loop to check if hosts has connection
 while [ ! -f "${CACHE_HOME}/swanctl.conf" ]; do
     echo "waiting for ${CACHE_HOME}/swanctl.conf ............"
     sleep 5
-done
-
-while true; do
-    if grep -q "${READY_FLAG}" "${CACHE_HOME}/hosts.ipsec"; then
-        echo "found connections: ${READY_FLAG}"
-        break
-    else
-        echo "not found connections: ${READY_FLAG}, waiting"
-        sleep 5
-    fi
 done
 
 # clean up old ipsecvpn certs and conf cache to use new in /etc/host-init-strongswan
@@ -46,9 +33,6 @@ rm -fr "/etc/swanctl/*"
 # debug config
 echo "cat ${CONF_HOME}/swanctl.conf ............"
 cat "${CONF_HOME}/swanctl.conf"
-
-echo "cat ${HOSTS_HOME} ............"
-cat "${HOSTS_HOME}"
 
 # start ipsecvpn server
 echo "Running ipsecvpn .............."
