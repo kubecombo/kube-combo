727706ee201302a65951810d93280d7d2f0c9590
zbb88888 <jmdxjsjgcxy@gmail.com>
2025-05-26 10:08:06 +0800

    add debugger crd webhook
    Signed-off-by: zbb88888 <jmdxjsjgcxy@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -184,6 +184,12 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Debugger")
 		os.Exit(1)
 	}
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = (&vpngwv1.Debugger{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "Debugger")
+			os.Exit(1)
+		}
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
