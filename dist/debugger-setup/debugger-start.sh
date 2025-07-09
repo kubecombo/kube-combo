89d55736460d7daa239200b6e6c81b41f9c612a4
zbb88888 <jmdxjsjgcxy@gmail.com>
2025-07-09 15:45:49 +0800

    add host run check list
    Signed-off-by: zbb88888 <jmdxjsjgcxy@gmail.com>

--- dist/debugger-setup/debugger-start.sh
+++ dist/debugger-setup/debugger-start.sh
@@ -16,6 +16,18 @@ echo "###### show env ######"
 env
 
 # 1. run script
+echo "###### run check list ######"
+if [ "$HOST_CHECK_LIST" = "true" ]; then
+  echo "Running host check list..."
+  if [ -f /check-list.sh ]; then
+    bash /check-list.sh
+  else
+    echo "No check-list.sh found. Skipping host check list."
+  fi
+else
+  echo "Host check list is disabled. Skipping."
+fi
+
 echo "###### run scripts ######"
 SCRIPTS_DIR="/scripts"
 if [ -d "$SCRIPTS_DIR" ]; then
