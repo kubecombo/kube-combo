c75766d88fc0191b06edc658251f03ed8acab2a3
zhaocongqi <1229896069@qq.com>
2025-07-16 17:08:52 +0800

    新增 kube-combo 控制器管理器的 RBAC 配置和相关资源，优化 Helm chart 结构
    Signed-off-by: zhaocongqi <1229896069@qq.com>

--- chart.mk
+++ chart.mk
@@ -6,3 +6,5 @@ chart:
 	jinja2 ./yamls/values.yaml.j2 ./yamls/values.yaml -D global_images_tag=v$(VERSION) > ./charts/kube-combo/values.yaml
 	$(KUSTOMIZE) build config/crd > ./charts/kube-combo/templates/kube-combo-crd.yaml
 	$(KUSTOMIZE) build yamls/rbac > ./charts/kube-combo/templates/kube-combo-rbac.yaml
+	$(KUSTOMIZE) build yamls/default > ./charts/kube-combo/templates/kube-combo-controller.yaml
+	@cat ./yamls/manager/append-nodeSelector.yaml >> ./charts/kube-combo/templates/kube-combo-controller.yaml
