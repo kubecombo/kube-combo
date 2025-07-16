623fe8f9fc6dd7aaf4e0f3f27be59f63fa601850
zhaocongqi <1229896069@qq.com>
2025-07-16 14:48:54 +0800

    新增 RBAC 配置，优化服务账户和角色绑定，支持动态命名空间
    Signed-off-by: zhaocongqi <1229896069@qq.com>

--- chart.mk
+++ chart.mk
@@ -5,4 +5,4 @@ chart:
 	jinja2 ./yamls/Chart.yaml.j2 -D app_version=v$(VERSION) > ./charts/kube-combo/Chart.yaml
 	jinja2 ./yamls/values.yaml.j2 ./yamls/values.yaml -D global_images_tag=v$(VERSION) > ./charts/kube-combo/values.yaml
 	$(KUSTOMIZE) build config/crd > ./charts/kube-combo/templates/kube-combo-crd.yaml
-	$(KUSTOMIZE) build config/rbac > ./charts/kube-combo/templates/kube-combo-rbac.yaml
+	$(KUSTOMIZE) build yamls/rbac > ./charts/kube-combo/templates/kube-combo-rbac.yaml
