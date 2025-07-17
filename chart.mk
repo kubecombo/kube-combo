879bcf6e7ae8146f44ad6d1ab843b4c273fb7f03
zhaocongqi <1229896069@qq.com>
2025-07-17 14:12:31 +0800

    优化 Makefile 中的 jinja2 目标，确保工具二进制文件的正确安装
    Signed-off-by: zhaocongqi <1229896069@qq.com>

--- chart.mk
+++ chart.mk
@@ -1,9 +1,17 @@
 # Makefile for generating helm chart
 
+## Tool Binaries
+JINJA2 ?= $(LOCALBIN)/jinja2/bin/jinja2
+
+.PHONY: jinja2
+jinja2: 
+	test -s $(LOCALBIN)/jinja2 || \
+	pip install --target $(LOCALBIN)/jinja2 jinja2-cli
+
 .PHONY: chart
-chart: 
-	jinja2 ./yamls/Chart.yaml.j2 -D app_version=v$(VERSION) > ./charts/kube-combo/Chart.yaml
-	jinja2 ./yamls/values.yaml.j2 ./yamls/values.yaml -D global_images_tag=v$(VERSION) > ./charts/kube-combo/values.yaml
+chart: jinja2 kustomize
+	$(JINJA2) ./yamls/Chart.yaml.j2 -D app_version=v$(VERSION) > ./charts/kube-combo/Chart.yaml
+	$(JINJA2) ./yamls/values.yaml.j2 ./yamls/values.yaml -D global_images_tag=v$(VERSION) > ./charts/kube-combo/values.yaml
 	$(KUSTOMIZE) build config/crd > ./charts/kube-combo/templates/kube-combo-crd.yaml
 	$(KUSTOMIZE) build yamls/rbac > ./charts/kube-combo/templates/kube-combo-rbac.yaml
 	$(KUSTOMIZE) build yamls/default > ./charts/kube-combo/templates/kube-combo-controller.yaml
