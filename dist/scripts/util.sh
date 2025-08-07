f8ba69124ef3cec0f6dc78eb44e93d71898f8658
zhaocongqi <1229896069@qq.com>
2025-08-07 11:27:26 +0800

    add network-card.j2 and common tools
    Signed-off-by: zhaocongqi <1229896069@qq.com>

---
+++ dist/scripts/util.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+generate_yaml_header() {
+    cat <<EOF
+nodename: "$(hostname)"
+timestamp: "$(date +%Y-%m-%dT%H:%M:%SZ)"
+EOF
+}
+
+generate_yaml_detection() {
+    if [ "$#" -ne 1 ]; then
+        echo "用法: generate_yaml_detection <section_name>" >&2
+        return 1
+    fi
+
+    local section_name="$1"
+    cat <<EOF
+$section_name:
+EOF
+}
+
+generate_yaml_entry() {
+    if [ "$#" -ne 3 ]; then
+        echo "用法: generate_yaml_entry <key> <value> <err>" >&2
+        return 1
+    fi
+
+    local key="$1"
+    local value="$2"
+    local err="$3"
+
+    cat <<EOF
+  - key: $key
+    value: "$value"
+    err: "$err"
+EOF
+}
