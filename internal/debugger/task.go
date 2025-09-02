e2bb3039059ae92298994990c18870f6a1a9e645
zhaocongqi <1229896069@qq.com>
2025-09-02 17:35:12 +0800

    add debug scripts
    Signed-off-by: zhaocongqi <1229896069@qq.com>

--- internal/debugger/task.go
+++ internal/debugger/task.go
@@ -23,54 +23,57 @@ type Task struct {
 
 // Task constant mapping
 var TaskMap = map[string]Task{
-	// cpu
-	"CPU_MODEL":       {Script: "/runAt/demo.sh", Args: ""},
-	"CPU_USAGE_RATE":  {Script: "check_cpu_usage.sh", Args: "--interval 5s"},
-	"CPU_TEMPERATURE": {Script: "check_cpu_temperature.sh", Args: ""},
-	"CPU_LOAD":        {Script: "check_cpu_load.sh", Args: "--avg 1"},
-
 	// raid
-	"RAID_CARD_STATUS": {Script: "check_raid_card_status.sh", Args: ""},
+	"RAID_CARD_STATUS": {Script: "", Args: ""},
+
+	// cpu
+	"CPU_MODEL":       {Script: "/runAt/cpu/CPU_Detection.sh", Args: ""},
+	"CPU_USAGE_RATE":  {Script: "/runAt/cpu/CPU_Detection.sh", Args: ""},
+	"CPU_TEMPERATURE": {Script: "/runAt/cpu/CPU_Detection.sh", Args: ""},
+	"CPU_LOAD":        {Script: "/runAt/cpu/CPU_Detection.sh", Args: ""},
 
 	// network card
-	"NETWORK_PORT_PACKET_LOSS":     {Script: "check_network_port_packet_loss.sh", Args: "--count 10"},
-	"NETWORK_PORT_CONNECTION_MODE": {Script: "check_network_port_connection_mode.sh", Args: ""},
-	"FULL_DUPLEX_MODE":             {Script: "check_full_duplex_mode.sh", Args: ""},
-	"NETWORK_PORT_SPEED":           {Script: "check_network_port_speed.sh", Args: ""},
-	"NETWORK_CARD_CONFLICT":        {Script: "check_network_card_conflict.sh", Args: ""},
-	"UNPLUGGED_AND_DISCONNECTION":  {Script: "check_unplugged_and_disconnection.sh", Args: ""},
+	"NETWORK_PORT_PACKET_LOSS":     {Script: "/runAt/network-card/network-card-detection.sh", Args: ""},
+	"NETWORK_PORT_CONNECTION_MODE": {Script: "/runAt/network-card/network-card-detection.sh", Args: ""},
+	"FULL_DUPLEX_MODE":             {Script: "/runAt/network-card/network-card-detection.sh", Args: ""},
+	"NETWORK_PORT_SPEED":           {Script: "/runAt/network-card/network-card-detection.sh", Args: ""},
+	"NETWORK_CARD_CONFLICT":        {Script: "/runAt/network-card/network-card-detection.sh", Args: ""},
+	"UNPLUGGED_AND_DISCONNECTION":  {Script: "/runAt/network-card/network-card-detection.sh", Args: ""},
 
 	// memory
-	"MEMORY_FREQUENCY":              {Script: "check_memory_frequency.sh", Args: ""},
-	"MEMORY_MANUFACTURER":           {Script: "check_memory_manufacturer.sh", Args: ""},
-	"MEMORY_READ_WRITE_PERFORMANCE": {Script: "check_memory_read_write_performance.sh", Args: "--test 1G"},
-	"MEMORY_LOOSENING_ANOMALY":      {Script: "check_memory_loosening_anomaly.sh", Args: ""},
-	"MEMORY_SIZE_ANOMALY":           {Script: "check_memory_size_anomaly.sh", Args: ""},
-	"MEMORY_USAGE_RATE":             {Script: "check_mem_usage.sh", Args: "--unit MB"},
+	"MEMORY_FREQUENCY":              {Script: "/runAt/memory/check_memory5.sh", Args: ""},
+	"MEMORY_MANUFACTURER":           {Script: "/runAt/memory/check_memory5.sh", Args: ""},
+	"MEMORY_READ_WRITE_PERFORMANCE": {Script: "/runAt/memory/check_memory5.sh", Args: ""},
+	"MEMORY_LOOSENING_ANOMALY":      {Script: "/runAt/memory/check_memory5.sh", Args: ""},
+	"MEMORY_SIZE_ANOMALY":           {Script: "/runAt/memory/check_memory5.sh", Args: ""},
+	"MEMORY_USAGE_RATE":             {Script: "/runAt/memory/check_memory5.sh", Args: ""},
 
-	// disk
-	"DISK_STATUS":        {Script: "check_disk_status.sh", Args: ""},
-	"DISK_BUSYNESS":      {Script: "check_disk_busyness.sh", Args: "--interval 5s"},
-	"SSD_LIFESPAN":       {Script: "check_ssd_lifespan.sh", Args: ""},
-	"SSD_INTERFACE_MODE": {Script: "check_ssd_interface_mode.sh", Args: ""},
-	"DISK_PERFORMANCE":   {Script: "check_disk_performance.sh", Args: "--test sequential"},
-	"IO_ERROR":           {Script: "check_io_error.sh", Args: ""},
-	"DISK_USAGE":         {Script: "check_disk_usage.sh", Args: "/dev/sda1"},
+	// basic disk function
+	"DISK_STATUS":        {Script: "/runAt/basic-disk-function/check_disk2.sh", Args: ""},
+	"DISK_BUSYNESS":      {Script: "/runAt/basic-disk-function/check_disk2.sh", Args: ""},
+	"SSD_LIFESPAN":       {Script: "/runAt/basic-disk-function/check_disk2.sh", Args: ""},
+	"SSD_INTERFACE_MODE": {Script: "/runAt/basic-disk-function/check_disk2.sh", Args: ""},
+	"DISK_PERFORMANCE":   {Script: "/runAt/basic-disk-function/check_disk2.sh", Args: ""},
+	"IO_ERROR":           {Script: "/runAt/basic-disk-function/check_disk2.sh", Args: ""},
+	"DISK_USAGE":         {Script: "/runAt/basic-disk-function/check_disk2.sh", Args: ""},
 
-	// control node
-	"OFFLINE_CONTROL_NODE":                 {Script: "check_offline_control_node.sh", Args: ""},
-	"CONTROL_NODE_BASIC_COMPONENT":         {Script: "check_control_node_basic_component.sh", Args: ""},
-	"CLUSTER_TIME_SYNCHRONIZATION_SERVICE": {Script: "check_cluster_time_synchronization.sh", Args: ""},
-	"CONTROL_NODE_MANAGEMENT_SERVICE":      {Script: "check_control_node_management_service.sh", Args: ""},
+	// system partition
+	"SYSTEM_PARTITION": {Script: "/runAt/system-partition/system-partition.sh", Args: ""},
+
+	// control node status
+	"OFFLINE_CONTROL_NODE":                 {Script: "", Args: ""},
+	"CONTROL_NODE_BASIC_COMPONENT":         {Script: "", Args: ""},
+	"CLUSTER_TIME_SYNCHRONIZATION_SERVICE": {Script: "", Args: ""},
+	"CONTROL_NODE_MANAGEMENT_SERVICE":      {Script: "", Args: ""},
 
 	// network configuration
-	"STORAGE_COMMUNICATION_NETWORK_PORT_CONNECTIVITY": {Script: "check_storage_network_connectivity.sh", Args: ""},
-	"LINK_AGGREGATION_CONNECTIVITY":                   {Script: "check_link_aggregation_connectivity.sh", Args: ""},
-	"LATENCY":                                         {Script: "check_network_latency.sh", Args: "--ping-count 5"},
-	"IP_CONFLICT":                                     {Script: "check_ip_conflict.sh", Args: ""},
-	"NEGOTIATED_RATE":                                 {Script: "check_negotiated_rate.sh", Args: ""},
-	"MTU_CONSISTENCY":                                 {Script: "check_mtu_consistency.sh", Args: ""},
-	"NETWORK_PORT_AGGREGATION_MODE_CONSISTENCY":       {Script: "check_port_aggregation_mode_consistency.sh", Args: ""},
+	"STORAGE_COMMUNICATION_NETWORK_PORT_CONNECTIVITY": {Script: "/runAt/network-configuration/network-configuration-detection.sh", Args: ""},
+	"LINK_AGGREGATION_CONNECTIVITY":                   {Script: "/runAt/network-configuration/network-configuration-detection.sh", Args: ""},
+	"LATENCY":                                         {Script: "/runAt/network-configuration/network-configuration-detection.sh", Args: ""},
+	"IP_CONFLICT":                                     {Script: "/runAt/network-configuration/network-configuration-detection.sh", Args: ""},
+	"NEGOTIATED_RATE":                                 {Script: "/runAt/network-configuration/network-configuration-detection.sh", Args: ""},
+	"MTU_CONSISTENCY":                                 {Script: "/runAt/network-configuration/network-configuration-detection.sh", Args: ""},
+	"NETWORK_PORT_AGGREGATION_MODE_CONSISTENCY":       {Script: "/runAt/network-configuration/network-configuration-detection.sh", Args: ""},
 }
 
 func loadDetection(filePath string) (*Detection, error) {
