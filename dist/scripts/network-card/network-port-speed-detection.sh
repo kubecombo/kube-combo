1140a98976896c2635e60a3384f10c9476429e0b
zhaocongqi <1229896069@qq.com>
2025-09-16 16:22:56 +0800

    add scripts for bond duplex mode detection
    Signed-off-by: zhaocongqi <1229896069@qq.com>

--- dist/scripts/network-card/network-port-speed-detection.sh
+++ dist/scripts/network-card/network-port-speed-detection.sh
@@ -2,3 +2,10 @@
 
 # shellcheck disable=SC1091
 source "$(dirname "${BASH_SOURCE[0]}")/../util/log.sh"
+# shellcheck disable=SC1091
+source "$(dirname "${BASH_SOURCE[0]}")/../util/util.sh"
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "${DIR}" || exit
+
+YAML=$(generate_yaml_detection "network_port_speed_results")$'\n'
--- dist/scripts/network-card/unplugged-and-disconnection-detection.sh
+++ dist/scripts/network-card/unplugged-and-disconnection-detection.sh
@@ -2,3 +2,10 @@
 
 # shellcheck disable=SC1091
 source "$(dirname "${BASH_SOURCE[0]}")/../util/log.sh"
+# shellcheck disable=SC1091
+source "$(dirname "${BASH_SOURCE[0]}")/../util/util.sh"
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "${DIR}" || exit
+
+YAML=$(generate_yaml_detection "unplugged_and_disconnection_results")$'\n'
