df60a4a2725113d2307757b77a39dd60438efa24
zhaocongqi <1229896069@qq.com>
2025-09-23 10:38:00 +0800

    fix log output
    Signed-off-by: zhaocongqi <1229896069@qq.com>

--- dist/scripts/util/log.sh
+++ dist/scripts/util/log.sh
@@ -19,40 +19,40 @@ esac
 function log_debug() {
     # shellcheck disable=SC2124
     content="[DEBUG] $(date '+%Y-%m-%d %H:%M:%S') $@"
-    [ "$LOG_FLAG" == "true" ] && echo "$content" >>"$LOG_FILE"
-    [ "$level_num" -le 1 ] && echo -e "\033[90m" "${content}" "\033[0m"
+    [ "$LOG_FLAG" == "true" ] && echo "$content" >>"$LOG_FILE" || true
+    [ "$level_num" -le 1 ] && echo -e "\033[90m" "${content}" "\033[0m" || true
 }
 
 # 信息日志
 function log_info() {
     # shellcheck disable=SC2124
     content="[INFO] $(date '+%Y-%m-%d %H:%M:%S') $@"
-    [ "$LOG_FLAG" == "true" ] && echo "$content" >>"$LOG_FILE"
-    [ "$level_num" -le 2 ] && echo -e "\033[32m" "${content}" "\033[0m"
+    [ "$LOG_FLAG" == "true" ] && echo "$content" >>"$LOG_FILE" || true
+    [ "$level_num" -le 2 ] && echo -e "\033[32m" "${content}" "\033[0m" || true
 }
 
 # 警告日志
 function log_warn() {
     # shellcheck disable=SC2124
     content="[WARN] $(date '+%Y-%m-%d %H:%M:%S') $@"
-    [ "$LOG_FLAG" == "true" ] && echo "$content" >>"$LOG_FILE"
-    [ "$level_num" -le 3 ] && echo -e "\033[33m" "${content}" "\033[0m"
+    [ "$LOG_FLAG" == "true" ] && echo "$content" >>"$LOG_FILE" || true
+    [ "$level_num" -le 3 ] && echo -e "\033[33m" "${content}" "\033[0m" || true
 }
 
 # 错误日志
 function log_err() {
     # shellcheck disable=SC2124
     content="[ERROR] $(date '+%Y-%m-%d %H:%M:%S') $@"
-    [ "$LOG_FLAG" == "true" ] && echo "$content" >>"$LOG_FILE"
-    [ "$level_num" -le 4 ] && echo -e "\033[31m" "${content}" "\033[0m"
+    [ "$LOG_FLAG" == "true" ] && echo "$content" >>"$LOG_FILE" || true
+    [ "$level_num" -le 4 ] && echo -e "\033[31m" "${content}" "\033[0m" || true
 }
 
 # 打印脚本执行结果总览
 function log_result() {
     # shellcheck disable=SC2124
     content="[RESULT] $(date '+%Y-%m-%d %H:%M:%S') $@"
-    [ "$LOG_FLAG" == "true" ] && echo "$content" >>"$LOG_FILE"
-    [ "$level_num" -le 5 ] && echo -e "\033[36m" "${content}" "\033[0m"
+    [ "$LOG_FLAG" == "true" ] && echo "$content" >>"$LOG_FILE" || true
+    [ "$level_num" -le 5 ] && echo -e "\033[36m" "${content}" "\033[0m" || true
 }
 
 # 打印文件日志
